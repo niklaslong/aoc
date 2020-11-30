f1a7ff3ed4631530c3a9d458228858c8ff5d22e5
Niklas Long <niklas@long.ch>
2020-11-30 14:31:04 +0100

    Day 2 part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -15,10 +15,10 @@ fn main() {
     // Runs the computation based on the opcodes.
     memory.compute();
 
-    println!("{:?}", memory);
+    println!("{:?}", memory.0[0]);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Memory(Vec<u64>);
 
 use std::num::ParseIntError;
@@ -63,6 +63,29 @@ impl Memory {
             self.0[write_address] = res;
         }
     }
+
+    fn noun_and_verb(&self, output: u64) -> (u64, u64) {
+        let mut noun_and_verb = (0u64, 0u64);
+
+        'outer: for i in 0..99 {
+            for j in 0..99 {
+                let mut memory = self.clone();
+
+                noun_and_verb = (i, j);
+
+                memory.0[1] = i;
+                memory.0[2] = j;
+
+                memory.compute();
+
+                if memory.0[0] == output {
+                    break 'outer;
+                };
+            }
+        }
+
+        noun_and_verb
+    }
 }
 
 #[cfg(test)]
@@ -83,5 +106,25 @@ mod tests {
         memory.compute();
 
         assert_eq!(memory.0, vec![30, 1, 1, 4, 2, 5, 6, 0, 99])
+    }
+
+    #[test]
+    fn finds_noun_and_verb_for_output() {
+        let output: u64 = 19690720;
+        let memory = Memory(vec![
+            1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23,
+            27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6,
+            51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71,
+            75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9,
+            99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119,
+            1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2,
+            139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0,
+        ]);
+
+        let (noun, verb) = memory.noun_and_verb(output);
+
+        // These are the correct values.
+        assert_eq!(noun, 82);
+        assert_eq!(verb, 50);
     }
 }
